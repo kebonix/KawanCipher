6ef153392c4dc4eea94c068ff6a24586ac3f15c9
Kawan <kawanbraz123@gmail.com>
2016-05-15 16:57:41 -0300

    implementando os métodos de cifrar e decifrar

--- kawan_cipher.py
+++ kawan_cipher.py
@@ -1,20 +1,32 @@
 letras = {"A":"N","B":"O","C":"P","D":"Q","E":"R","F":"S",
 "G":"T","H":"U","I":"V","J":"W","K":"X","L":"Y","M":"Z"}
 
-class KawanCipher
+class KawanCipher:
 
   def __int__():
-   self.mensagem_original
-   self.mensagem_cifrada
 
-  def inserir_msg_original:
-    self.mensagem_original = input("Digite a mensagem que você deseja cifrar: ")
-
-  def inserir_msg_cifrada:
-    self.mensagem_cifrada = input("Digite a mensagem que você deseja decifrar: ")
- 
   def cifrar(mensagem):
-    
-
+    texto_cifrado = ""
+    for letra in mensagem:
+      if letra == " ":
+        texto_cifrado += " "
+      else:
+        texto_cifrado += letras[letra.upper()]
+    return texto_cifrado
+      
   def decifrar(mensagem):
+    texto_decifrado = ""
+    for letra in mensagem:
+      if letra == " ":
+        texto_decifrado += " "
+      else:
+        for chave,valor in letras.items():
+          if letra == valor:
+            texto_decifrado += chave
+    return texto_decifrado
+
+cipher = KawanCipher
+print(texto_cifrado = cipher.cifrar(input("\nDigite a mensagem que você deseja cifrar: ")))
+print(texto_decifrado = cipher.decifrar(input("\nDigite a mensagem que você deseja decifrar: ")))
+
     
