0bccb8690b9ba31c8f360f23a73cefebf0202160
Gabriel Augusto <gabriel.a.b@outlook.com>
2016-05-15 16:20:52 -0300

    refactoring cipher

--- kawan_cipher.py
+++ kawan_cipher.py
@@ -1,9 +1,20 @@
 letras = {"A":"N","B":"O","C":"P","D":"Q","E":"R","F":"S",
 "G":"T","H":"U","I":"V","J":"W","K":"X","L":"Y","M":"Z"}
-palavra = []
-letra =(input("Digite uma letra da mensagem(digite 0 para encerrar): ")
-palavra.append(letras[letra])
-while letra =! "0":
-    letra =(input("Digite uma letra da mensagem(digite 0 para encerrar): ")
-    palavra.append(letras[letra])
+
+class KawanCipher
+
+  def __int__():
+   self.mensagem_original
+   self.mensagem_cifrada
+
+  def inserir_msg_original:
+    self.mensagem_original = input("Digite a mensagem que você deseja cifrar: ")
+
+  def inserir_msg_cifrada:
+    self.mensagem_cifrada = input("Digite a mensagem que você deseja decifrar: ")
+ 
+  def cifrar(mensagem):
+    
+
+  def decifrar(mensagem):
     
