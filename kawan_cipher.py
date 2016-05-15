aa342e01292be865b0dcca676ac9afe729cf89fb
Kawan <kawanbraz123@gmail.com>
2016-05-15 15:50:19 -0300

    primeiro commit

---
+++ kawan_cipher.py
@@ -0,0 +1,9 @@
+letras = {"A":"N","B":"O","C":"P","D":"Q","E":"R","F":"S",
+"G":"T","H":"U","I":"V","J":"W","K":"X","L":"Y","M":"Z"}
+palavra = []
+letra =(input("Digite uma letra da mensagem(digite 0 para encerrar): ")
+palavra.append(letras[letra])
+while letra =! "0":
+    letra =(input("Digite uma letra da mensagem(digite 0 para encerrar): ")
+    palavra.append(letras[letra])
+    
